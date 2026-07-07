6bb0d5f4c172ed4e1596d6dcdeb5edc790359fee
liaoshihan <liaoshihan176@gmail.com>
2026-07-07 16:04:58 +0800

    fix(backend/config): 修复环境变量配置文件的路径错误
    调整了.env文件的加载路径，修正相对路径层级错误以正确加载配置文件

--- backend/src/platform/config/env.ts
+++ backend/src/platform/config/env.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import { fileURLToPath } from 'node:url'
 
-config({ path: fileURLToPath(new URL('../../../.env', import.meta.url)) })
+config({ path: fileURLToPath(new URL('../../../../.env', import.meta.url)) })
 
 interface EnvConfig {
   port: number
